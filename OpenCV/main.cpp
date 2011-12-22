f947d03ea24e47287502b2d629ab9883ba0ec8b2
christian <christian@MacBook-Pro.local>
2011-12-21 23:55:01 -0600

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by Christian Roman Mendoza on 21/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
